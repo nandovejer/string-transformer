147ed5ecdcff3c54ff7859bfec9c67c3c9061b64
johndoe <johndoe@example.com>
2023-12-29 20:15:55 +0100

    window object and refactor

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -12,6 +12,11 @@ const outputTextarea = form?.querySelector("#output");
 const copyOutputBtn = form?.querySelector("#copyOutputBtn");
 const resetInputBtn = form?.querySelector("#resetInputBtn");
 
+const transformerStatus = {
+  set: (value = null) => (window.transformerId = value),
+  get: () => window.transformerId,
+};
+
 const executeConverter = ({ mode, str, transformers, outputTextarea }) => {
   outputTextarea.value = strTo({ mode, str, transformers });
 };
@@ -38,20 +43,21 @@ const updateFormMessage = ({
 };
 
 const initEvents = () => {
-  const transformerId = transformerStatusEvent.get();
-  if (!transformerId) return;
-  
-  form.addEventListener("submit", (event) => {
+  const transformerId = transformerStatus.get();
+
+  const submitForm = (event) => {
     event.preventDefault();
+    console.log("submit form");
     const inputValue = form.elements.input.value;
     updateForm({ transformerId, inputValue });
-  });
+  };
 
-  inputTextarea.addEventListener("change", (event) => {
+  const inputTextareaChange = (event) => {
     event.preventDefault();
+    console.log("change input textarea");
     const inputValue = event.target.value;
     updateForm({ transformerId, inputValue });
-  });
+  };
 
   copyValue({
     btn: copyOutputBtn,
@@ -63,10 +69,13 @@ const initEvents = () => {
     inputReset: inputTextarea,
     outputReset: outputTextarea,
   });
+
+  form.addEventListener("submit", submitForm);
+  inputTextarea.addEventListener("change", inputTextareaChange);
 };
 
 const updateForm = ({ inputValue }) => {
-  const transformerId = transformerStatusEvent.get();
+  const transformerId = transformerStatus.get();
   if (
     !transformerId ||
     !form ||
@@ -77,8 +86,6 @@ const updateForm = ({ inputValue }) => {
     !outputLabel
   )
     return;
-
-  initEvents();
 
   updateFormMessage({
     transformerId,
@@ -94,18 +101,6 @@ const updateForm = ({ inputValue }) => {
     transformers: transformers,
     outputTextarea: outputTextarea,
   });
-};
-
-const transformerStatusEvent = {
-  currentTransformerId: null,
-  dispatch: (value = null) => {
-    transformerStatusEvent.currentTransformerId = value;
-    const transformerIdEvent = new CustomEvent("updateTransformerId", {
-      detail: { transformerId: value },
-    });
-    document.dispatchEvent(transformerIdEvent);
-  },
-  get: () => transformerStatusEvent.currentTransformerId,
 };
 
 function createMenu(data) {
@@ -129,7 +124,7 @@ function createMenu(data) {
   const selector = parsedHtml.querySelector("select");
   selector.addEventListener("change", (event) => {
     console.log("change");
-    transformerStatusEvent.dispatch(event.target.value);
+    transformerStatus.set(event.target.value);
     if (inputTextarea) {
       updateForm({ inputValue: inputTextarea.value });
     }
@@ -139,154 +134,10 @@ function createMenu(data) {
 }
 
 function app() {
-  transformerStatusEvent.dispatch(null);
+  transformerStatus.set();
   createMenu(transformers);
   updateForm({ inputValue: inputTextarea.value });
+  initEvents();
 }
 
 export default app;
-
-/*
-const form = document.querySelector("#formApp");
-const title = form?.querySelector(".js-st-titleSelected");
-const description = form?.querySelector(".js-st-descriptionSelected");
-const inputLabel = form?.querySelector("label[for=input]");
-const inputTextarea = form?.querySelector("#input");
-const outputLabel = form?.querySelector("label[for=output]");
-const outputTextarea = form?.querySelector("#output");
-
-const executeConverter = ({ mode, str, transformers, outputTextarea }) => {
-  outputTextarea.value = strTo({ mode, str, transformers });
-};
-
-const updateFormMessage = ({
-  transformerId,
-  title,
-  description,
-  inputLabel,
-  outputLabel,
-}) => {
-  title.textContent = msg[transformerId]
-    ? msg[transformerId].title
-    : msg.defaultConverter.title;
-  description.textContent = msg[transformerId]
-    ? msg[transformerId].description
-    : msg.defaultConverter.description;
-  inputLabel.textContent = msg[transformerId]
-    ? msg[transformerId].inputLabel
-    : msg.defaultConverter.inputLabel;
-  outputLabel.textContent = msg[transformerId]
-    ? msg[transformerId].outputLabel
-    : msg.defaultConverter.outputLabel;
-};
-
-const updateForm = ({ transformerId, currentValue }) => {
-  if (
-    !transformerId ||
-    !form ||
-    !title ||
-    !inputTextarea ||
-    !outputTextarea ||
-    !inputLabel ||
-    !outputLabel
-  )
-    return;
-
-  updateFormMessage({
-    transformerId,
-    title,
-    description,
-    inputLabel,
-    outputLabel,
-  });
-
-  executeConverter({
-    mode: transformerId,
-    str: currentValue,
-    transformers: transformers,
-    outputTextarea: outputTextarea,
-  });
-};
-
-function createMenu(data) {
-  const output = document.querySelector("#stMenu");
-  if (!output || !data) return;
-  const parser = new DOMParser();
-
-  // Create menu
-  const menuHtml = () => {
-    const _data = Object.keys(data);
-    let listItems = "";
-    _data.forEach((item) => {
-      listItems += `<option value="${item}">${item}</option>`;
-    });
-    const dropdownStructure = `<select><option value="" disabled selected>Select Transformer</option>${listItems}</select>`;
-    return dropdownStructure;
-  };
-
-  // Init Event
-  const parsedHtml = parser.parseFromString(menuHtml(), "text/html");
-  const selector = parsedHtml.querySelector("select");
-  selector.addEventListener("change", (event) => {
-    event.preventDefault();
-    updateForm({
-      transformId: event.target.value,
-      currentValue: form.elements.input.value,
-    });
-  });
-
-  output.appendChild(selector);
-}
-
-const initEvents = () => {
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const currentValue = form.elements.input.value;
-    updateForm({ transformerId, currentValue });
-  });
-
-  inputTextarea.addEventListener("change", (event) => {
-    event.preventDefault();
-    const currentValue = event.target.value;
-    updateForm({ transformerId, currentValue });
-  });
-
-  copyValue({
-    btn: document.querySelector("#copyOutputBtn"),
-    outputCopy: outputTextarea,
-  });
-
-  resetInput({
-    btn: document.querySelector("#resetInputBtn"),
-    inputReset: inputTextarea,
-    outputReset: outputTextarea,
-  });
-};
-
-// Custom Event Like global status
-const transformerStatusEvent = {
-  get: () => {
-    document.addEventListener("updateTransformerId", (event) => {
-      console.log(event.detail.transformId);
-      return event.detail.transformId;
-    });
-  },
-  set: () => {},
-  dispatch: () => {
-    const transformerIdEvent = new CustomEvent("updateTransformerId", {
-      detail: { transformerId: null },
-    });
-    document.dispatchEvent(transformerIdEvent);
-  },
-};
-
-function app() {
-  transformerStatusEvent.dispatch();
-  console.log(transformerStatusEvent.get());
-
-  // initEvents();
-  // createMenu(transformers);
-  // console.log("app");
-}
-export default app;
-*/
--- src/js/modules/utils.js
+++ src/js/modules/utils.js
@@ -1,23 +1,31 @@
 const copyValue = ({ btn, outputCopy }) => {
-  btn.addEventListener("click", (event) => {
+
+  const copyToClipboard = (event) => {
     event.preventDefault();
+    const outputValue = outputCopy.value;
+    if(!outputValue) return;
+    
     navigator.clipboard
-      .writeText(outputCopy.value)
+      .writeText(outputValue)
       .then(() => {
-        console.log("Text in clipboard: " + outputCopy.value);
+        console.log("Text in clipboard: " + outputValue);
       })
       .catch((err) => {
         console.error("Error clipboard: ", err);
       });
-  });
+  };
+
+  btn.addEventListener("click", copyToClipboard);
 };
 
 const resetInput = ({ btn, inputReset, outputReset }) => {
-  btn.addEventListener("click", (event) => {
+  const reset = (event) => {
     event.preventDefault();
     inputReset.value = "";
     outputReset.value = "";
-  });
+  };
+
+  btn.addEventListener("click", reset);
 };
 
 export { copyValue, resetInput };
